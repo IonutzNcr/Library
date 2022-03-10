9cd3f5c026591c20abdbc879918b93519e6d621c
Ionut <ionutzncr@gmail.com>
2022-03-10 18:36:04 +0100

    Struggling to implement remove book feture I'm nearly done but still so far

--- library.js
+++ library.js
@@ -1,10 +1,11 @@
 let myLibrary = [];
 
-function Book(title,author,page,read) {
+function Book(title,author,page,read,index) {
     this.title = title;
     this.author = author;
     this.page = page;
     this.read = read;
+    this.index = index;
   // the constructor...
 }
 
@@ -16,8 +17,9 @@ function addBookToLibrary() {
   const author = takeFromDom("#author");
   const pages = takeFromDom("#pages");
   const read = takeFromDom("#read");
+  const index = myLibrary.length
   if(title && author && pages){
-    const book = new Book(title,author,pages,read);
+    const book = new Book(title,author,pages,read,index);
     myLibrary.push(book)
   }
   
@@ -38,10 +40,15 @@ function displayBooks(){
   document.querySelectorAll("#library div").forEach(e=>e.remove())
 
   myLibrary.forEach(e=>{
-    createCard(e.title,e.author,e.page,e.read);
+    createCard(e.title,e.author,e.page,e.read,e.index);
   })
-  function createCard(title,author,page,read){
-    const card = document.createElement("div")
+  function createCard(title,author,page,read,index){
+    const card = document.createElement("div");
+    card.setAttribute("data-index",index);
+    const remove = document.createElement("button");
+    remove.textContent = "X";
+    remove.setAttribute("class","remove");
+
     const p_title = document.createElement("p")
     const p_author = document.createElement("p")
     const p_pages = document.createElement("p")
@@ -52,12 +59,17 @@ function displayBooks(){
     p_pages.textContent = `Pages : ${page}`
     p_read.textContent = `Did you read it? ${read}`
 
+    card.appendChild(remove)
     card.appendChild(p_title);
     card.appendChild(p_author);
     card.appendChild(p_pages);
     card.appendChild(p_read);
     document.querySelector("#library").appendChild(card);
   }
+
+  document.querySelectorAll(".remove").forEach(e=>{
+    e.addEventListener("click",removeBook)
+  })
 
 }
 
@@ -79,3 +91,16 @@ const buttonAdd = document.querySelector("#add")
 buttonAdd.addEventListener("click",toggleCLass);
 
 button.addEventListener("click",toggleCLass);
+
+/* Remove book feature */
+
+function removeBook(){
+  const index = +this.parentNode.dataset.index;
+  myLibrary.splice(index,1);
+  displayBooks();
+  
+}
+
+
+
+
