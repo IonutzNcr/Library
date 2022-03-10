5cab31b62af7b8bdcec70d3c525ca3da1bb3ad12
Ionut <ionutzncr@gmail.com>
2022-03-10 11:20:15 +0100

    Html css and js squellete

---
+++ library.js
@@ -0,0 +1,14 @@
+let myLibrary = [];
+
+function Book(title,page) {
+    this.title = title;
+    this.page = page;
+  // the constructor...
+}
+
+Book.prototype = {
+    takeTitle: function()
+}
+function addBookToLibrary() {
+  // do stuff here
+}
